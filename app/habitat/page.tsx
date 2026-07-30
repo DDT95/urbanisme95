d6eb6aaed8e40b84630a3139ee30cfe145220da1
Wilfried Koba <wilfriedkoba@device-374.home>
2026-07-30 18:59:25 +0200

    Mettre à jour le titre Logement et Habitat

--- app/habitat/page.tsx
+++ app/habitat/page.tsx
@@ -1,8 +1,7 @@
 export default function HabitatPage() {
   return (
     <main className="embedded-tool">
-      <iframe className="habitat-frame" src="https://ddt95.github.io/observatoire_bati/" title="Observatoire du bâti du Val-d’Oise" allow="geolocation" />
+      <iframe className="habitat-frame" src="https://ddt95.github.io/observatoire_bati/" title="Logement & Habitat du Val-d’Oise" allow="geolocation" />
     </main>
   );
 }
-
